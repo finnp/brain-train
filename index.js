d5e908e8d4c2f4bc2268573c0172e1c09ef4e68b
Finn <derfinn@gmail.com>
2014-08-27 15:16:51 +0200

    select rows

--- index.js
+++ index.js
@@ -3,22 +3,35 @@ var ldj = require('ldjson-stream')
 var fs = require('fs')
 var through = require('through2').obj
 var tempfile = require('tempfile')()
+var argv = require('minimist')(process.argv.slice(2))
 // tmp,cnt
-console.log(tempfile)
+console.dir(argv)
 
 process.stdin
   .pipe(ldj.parse())
   .pipe(through(function (data, enc, cb) {
-    var data = {
-      input: {
-        temp: Number(data.temp)
-      },
-      output: {
-        cnt: (Number(data.cnt) / 5000)
-      }
+    var dataio = {'input': {}, 'output': {}}
+    
+    if(argv.input) {
+      var keys = argv.input.split(',')
+      keys.forEach(function (key) {
+        dataio['input'][key] = data[key]
+      })
+    } else {
+      dataio['input'] = data['input']
     }
+    
+    if(argv.output) {
+      var keys = argv.output.split(',')
+      keys.forEach(function (key) {
+        dataio['output'][key] = data[key]
+      })
+    } else {
+      dataio['output'] = data['output']
+    }  
+    
     // console.log(data)
-    cb(null, data)
+    cb(null, dataio)
   }))
   .pipe(ldj.serialize())
   .pipe(fs.createWriteStream(tempfile))
--- readme.md
+++ readme.md
@@ -3,10 +3,17 @@
 
 ## Usage
 ```
-cat data.ldjson | brain-train -input input,input2 --output color > network.json
+cat data.ldjson | brain-train --input input,input2 --output color > network.json
 ```
 
 Note that it will create a temporary file on your disk for process.stdin first
 to reuse the stream for the repeated iterations.
 
 Values for training data have to be between 0 and 1.
+
+## TODO
+
+Brain expects the data to be between 0 and 1. However most datasets have a wider
+range of values. Maybe it would be okay to take the max and min values of a row
+and scale the data to 0 and 1 between those.
+Maybe this could be done by a "normalize" module.
